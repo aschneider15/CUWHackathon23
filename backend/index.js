f5ab4dd3d868ecb90d07f13ab8fd081482da2744
aschneider15 <69209408+aschneider15@users.noreply.github.com>
2023-10-28 13:37:38 -0500

    Changed from NDK to Nostr-tools
    Better documentation

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,1 @@
-const NDK = require('ndk');
-
-ndkInstance = new NDK({
-    explicitRelayUrls: ["wss://relay.nostrss.re", "wss://nostr.lnproxy.org"],
-});
-
-console.log('Hello ');
-
+console.log('hi')
