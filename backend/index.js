35f5d932431700e0a65b453cb035a5e2de77174e
aschneider15 <69209408+aschneider15@users.noreply.github.com>
2023-10-28 13:50:26 -0500

    Initial utilization of nostr-tools

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log('hi')
+// imports nostr-tools from the node_modules folder.
+// "tools" is the object through which we can interact with
+// nostr-tools' functions.
+const tools = require('nostr-tools')
+
+let sk = tools.generatePrivateKey() // `sk` is a hex string
+let pk = tools.getPublicKey(sk) // `pk` is a hex string
+
+console.log(sk)
+console.log(pk)
